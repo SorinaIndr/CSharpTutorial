7c8bc480514119d5ddd354a238ce0c03bbfcbbad
SorinaIndr <ynd.sorina@yahoo.com>
2021-02-15 23:33:39 +0200

    FirstSeleniumTest

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+
+		[Test]
+		public void FirstTest()
+		{
+			IWebDriver Driver = new ChromeDriver();
+			Driver.Navigate().GoToUrl("https://www.codegrepper.com/code-examples/delphi/how+to+set+git+username+and+password+in+git+bash");
+
+		}
+	}
+}
